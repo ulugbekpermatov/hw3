795db6df0ed952ff82526adca470569905c4bbc6
ulugbekpermatov <d9911833@gmail.com>
2022-07-25 18:25:21 +0600

    Added all hw

---
+++ 1/script.js
@@ -0,0 +1,13 @@
+const collback = [23, 72, 73, 24, 15];
+let result = 0;
+
+for( i in collback){
+    result +=collback[i];
+    console.log(result);
+};
+
+let a = 50;
+while (a >= 0) {
+  console.log(a);
+  a--;
+}
